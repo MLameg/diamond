2f93a6fadbaf32a9c91a487d02b1775842b50e36
MLameg <MLameg12@gmail.com>
2018-02-22 20:26:14 -0500

    Add files via upload

---
+++ Diamond.java
@@ -0,0 +1,20 @@
+//***************************************************************************
+//  Diamond: Page 55  PP 1.9 
+//  By TJ Lameg
+//*************************************************************************** 
+
+public class Diamond 
+{
+    //***********************************************************************
+	//WRite a program that prints the following diamond shape.
+	//***********************************************************************
+	public static void main(String [] args){
+    System.out.println("    *    ");
+	System.out.println("   ***   ");
+	System.out.println("  *****  ");
+    System.out.println(" ******* ");
+    System.out.println("*********");
+    System.out.println(" ******* ");
+    System.out.println("  *****  ");
+	System.out.println("   ***   ");
+	System.out.println("    *    ");}}
